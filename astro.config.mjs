00bd0fd6900b41a5cad2ff1146c455d06af921f6
And-ers <accornect@mun.ca>
2024-07-24 12:17:07 -0230

    modified:   astro.config.mjs modified:   src/pages/index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import { astroImageTools } from "astro-imagetools";
-
-export default {
-  integrations: [astroImageTools],
-};
-
 // https://astro.build/config
-// export default defineConfig({});
+export default defineConfig({});
