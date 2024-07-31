780e382c1603124cb02150a1b3463ce1a4d580b8
And-ers <accornect@mun.ca>
2024-07-31 15:39:36 -0230

    modified:   astro.config.mjs new file:   src/layouts/SideNote.astro modified:   src/pages/posts/post-2024-07-17.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import rehypeKatex from 'rehype-katex'; // relevant
-import remarkMath from 'remark-math'; // relevant
-
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
     remarkPlugins: [remarkMath],
-    // relevant
-    rehypePlugins: [rehypeKatex] // relevant
-  }), react()]
+    rehypePlugins: [rehypeKatex]
+  }), 
+  react()]
 });
