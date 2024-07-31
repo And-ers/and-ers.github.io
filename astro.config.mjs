be22ce81d0b1de22a744fecc47201cb8c79a8f6f
And-ers <accornect@mun.ca>
2024-07-31 15:19:14 -0230

    modified:   astro.config.mjs modified:   package-lock.json modified:   package.json modified:   tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import rehypeKatex from 'rehype-katex'; // relevant
-import remarkMath from 'remark-math';   // relevant
+import remarkMath from 'remark-math'; // relevant
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
-    remarkPlugins: [remarkMath], // relevant
+    remarkPlugins: [remarkMath],
+    // relevant
     rehypePlugins: [rehypeKatex] // relevant
-  })],
+  }), react()]
 });
