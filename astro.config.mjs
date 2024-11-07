7f110e4e4c9539039fef94002e812b2d79e7f366
And-ers <accornect@mun.ca>
2024-11-07 12:55:07 -0500

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/src/pages',
   site: 'https://and-ers.github.io',
   integrations: [mdx({
     remarkPlugins: [remarkMath],
